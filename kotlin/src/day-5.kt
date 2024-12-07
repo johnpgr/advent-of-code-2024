54a4996f22b2a2cf1bbd3b01c18a9e543ae193f5
johnpgr <jp.greidinger@hotmail.com>
2024-12-07 11:44:43 -0300

    Add day5 solution

---
+++ kotlin/src/day-5.kt
@@ -0,0 +1,68 @@
+fun List<Int>.inOrder(rules: List<List<Int>>): Boolean {
+    for (rule in rules) {
+        val (a, b) = rule
+        if (contains(a) && contains(b)) {
+            if (indexOf(a) > indexOf(b)) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+fun List<Int>.sortWithRules(rules: List<List<Int>>): List<Int> {
+    tailrec fun sort(current: List<Int>, hasSwapped: Boolean): List<Int> = when {
+        !hasSwapped -> current
+        else -> {
+            val (newList, swapped) = rules.fold(Pair(current, false)) { (acc, didSwap), rule ->
+                val (first, second) = rule
+                when {
+                    !acc.contains(first) || !acc.contains(second) -> Pair(acc, didSwap)
+                    acc.indexOf(first) > acc.indexOf(second) -> {
+                        val swappedList = acc.toMutableList().apply {
+                            val firstIndex = indexOf(first)
+                            val secondIndex = indexOf(second)
+                            set(firstIndex, second)
+                            set(secondIndex, first)
+                        }
+                        Pair(swappedList, true)
+                    }
+                    else -> Pair(acc, didSwap)
+                }
+            }
+            sort(newList, swapped)
+        }
+    }
+    
+    return sort(this, true)
+}
+
+
+fun main() {
+    fun partOne(input: String): Int {
+        val splits = input.split("\n\n")
+        val pageOrderingRules = splits[0].lines().map { it.split("|").map(String::toInt) }
+        val pages = splits[1].lines().map { it.split(",").map(String::toInt) }
+        val correctlyOrdered = pages.filter { it.inOrder(pageOrderingRules) }
+        val middleSum = correctlyOrdered.fold(0) { acc, list -> acc + list[list.size / 2] }
+
+        return middleSum
+    }
+
+    fun partTwo(input: String): Int {
+        val splits = input.split("\n\n")
+        val pageOrderingRules = splits[0].lines().map { it.split("|").map(String::toInt) }
+        val pages = splits[1].lines().map { it.split(",").map(String::toInt) }
+        val incorrectlyOrdered = pages.filterNot { it.inOrder(pageOrderingRules) }
+        val ordered = incorrectlyOrdered.map { it.sortWithRules(pageOrderingRules) }
+        val middleSum = ordered.fold(0) { acc, list -> acc + list[list.size / 2] }
+
+        return middleSum
+    }
+
+
+    solve(::partOne, "day-5.test", 143)
+    solve(::partOne, "day-5")
+    solve(::partTwo, "day-5.test", 123)
+    solve(::partTwo, "day-5")
+}
--- kotlin/src/utils.kt
+++ kotlin/src/utils.kt
@@ -1,3 +1,4 @@
+import java.lang.management.ManagementFactory
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 
@@ -6,13 +7,23 @@ import kotlin.io.path.readText
  */
 fun readInput(name: String) = Path("input/$name.txt").readText().trim()
 
-fun solve(fn: (String) -> Int, input: String) {
-    val result = fn(readInput(input))
-    println(result)
-}
+fun getFunctionName(fn: (String) -> Int): String = fn.toString().split("function ")[1].split(" ")[0]
 
-fun solve(fn: (String) -> Int, input: String, assert: Int) {
+fun solve(fn: (String) -> Int, input: String, assert: Int? = null) {
+    val fnName = getFunctionName(fn)
+    val memoryMXBean = ManagementFactory.getMemoryMXBean()
+    System.gc()
+    val beforeUsedHeapMemory = memoryMXBean.heapMemoryUsage.used
+    val startTime = System.currentTimeMillis()
     val result = fn(readInput(input))
-    require(result == assert) { "Expected $assert but got $result" }
-    println(result)
+    val endTime = System.currentTimeMillis()
+    val runtimeMs = endTime - startTime
+    val afterUsedHeapMemory = memoryMXBean.heapMemoryUsage.used
+    val usedMemory = afterUsedHeapMemory - beforeUsedHeapMemory
+
+    assert?.let {
+        require(result == it) { "Expected $it but got $result" }
+    }
+
+    println("[$input]: $fnName -> $result (runtime: ${runtimeMs}ms, heap: ${usedMemory / 1024}MB)")
 }
