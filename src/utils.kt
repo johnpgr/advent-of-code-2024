85ff2e2a63b568327f15bc0be0b4b1d9e1b44d59
johnpgr <jp.greidinger@hotmail.com>
2024-12-13 14:40:10 -0300

    update solve()

--- src/utils.kt
+++ src/utils.kt
@@ -6,7 +6,7 @@ fun readInput(name: String) = Path("input/$name.txt").readText().trim()
 
 fun <Input, Output> getFunctionName(fn: (Input) -> Output): String = fn.toString().split("function ")[1].split(" ")[0]
 
-fun <Output> solve(fn: (String) -> Output, input: String, assert: Output? = null) {
+fun <Output> solve(fn: (String) -> Output, input: String, asserts: Output? = null) {
     val fnName = getFunctionName(fn)
     val inputData = readInput(input)
     val startTime = System.nanoTime()
@@ -17,7 +17,11 @@ fun <Output> solve(fn: (String) -> Output, input: String, assert: Output? = null
 
     val runtimeMs = (endTime - startTime) / 1_000_000.0
 
-    assert?.let { require(result == it) { "Expected $it but got $result" } }
+    asserts?.let { 
+        if(result != it) {
+            throw AssertionError("[$input]: $fnName -> Expected $it but got $result")
+        }
+    }
 
     println("[$input]: $fnName -> $result (runtime: ${String.format("%.2f", runtimeMs)}ms)")
 }
